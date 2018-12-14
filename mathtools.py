0852425f057201e1a3537e6f948b74fd6f84c75d
sandalf <saku.salo@live.fi>
2018-12-14 14:23:16 +0200

    new function mathtools.py

--- mathtools.py
+++ mathtools.py
@@ -36,3 +36,12 @@ def fib(n):
     else:
         return fib(n-1)+fib(n-2)
 
+def geometric(a, ratio, n):
+    '''Calculates the sum of a geometric serie of n elements.
+       A geometric sequence is of the form: a, a*r, a*r*r, a*r*r*r,...
+       n is the number of elements in the sequence.'''
+    #Get the geometric sequence
+    sequence = [a*(ratio**x) for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
+
