06c69aa015bd33cf76e536b57cbeba4686223426
sandalf <saku.salo@live.fi>
2018-12-14 14:00:25 +0200

    new function mathtools.py

--- mathtools.py
+++ mathtools.py
@@ -17,3 +17,12 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
